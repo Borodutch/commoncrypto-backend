aacee697e6d24a8fa552ce19fb39acd7c55b4273
backmeupplz <backmeupplz@gmail.com>
2021-08-06 16:11:25 -0700

    add ratelimit

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import * as bodyParser from 'koa-bodyparser'
 import { bootstrapControllers } from 'amala'
 import * as cors from '@koa/cors'
 import * as Router from 'koa-router'
+const ratelimit = require('koa-ratelimit')
+
+const db = new Map()
 
 export const app = new Koa()
 ;(async () => {
@@ -16,6 +19,16 @@ export const app = new Koa()
       controllers: [__dirname + '/controllers/*'],
       disableVersioning: true,
     })
+    app.use(
+      ratelimit({
+        duration: 1000,
+        max: 2,
+        errorMessage:
+          'Too many requests, please try again later (limit is 2 requests per second)',
+        driver: 'memory',
+        db,
+      })
+    )
     app.use(cors({ origin: '*' }))
     app.use(bodyParser())
     app.use(router.routes())
